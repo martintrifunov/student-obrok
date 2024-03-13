97ff72a8239d85a4620e46c26092cfada89ec4fe
martintrifunov <martintrifunov@proton.me>
2024-03-13 22:30:59 +0100

    Implement Client Side Routing

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,21 @@
-function App() {
-  return (
-    <p>Hello world!</p>
-  )
-}
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NotFound from "../pages/NotFound";
+import Login from "../pages/Login";
+import Map from "../pages/Map";
 
-export default App
+const App = () => {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/auth/login" element={<Login />} />
+          <Route path="/" element={<Map />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </>
+  );
+};
+
+export default App;
---
+++ client/src/pages/NotFound.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div>404 Drugar</div>
+  )
+}
+
+export default NotFound
