bacb3d322892e253e0fd05b06df85e6183cc0c73
martintrifunov <martintrifunov@proton.me>
2024-03-13 20:18:58 +0100

    Implement user register & login

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -10,9 +10,37 @@ router.post("/register", async (req, res) => {
 
   const user = await UserModel.findOne({ username });
 
-  res.json(user);
+  if (user) {
+    return res.status(409).json({ message: "User already exists!" });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const newUser = new UserModel({ username, password: hashedPassword });
+  await newUser.save();
+
+  res.status(200).json({ message: "User Registered Successfully!" });
 });
 
-router.post("/login");
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  const user = await UserModel.findOne({ username });
+
+  if (!user) {
+    return res.status(409).json({ message: "User does not exists!" });
+  }
+
+  const passwordValidator = await bcrypt.compare(password, user.password);
+
+  if (!passwordValidator) {
+    return res.status(401).json({ message: "Wrong password!" });
+  }
+
+  //TODO MAKE SECRET ENV VAR
+  const token = jwt.sign({ id: user._id }, "secret");
+
+  res.status(200).json({ token, uerId: user._id });
+});
 
 export { router as userRouter };
