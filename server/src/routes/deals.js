db068813b3af66efc75d0900a3bd672a3fd105dc
martintrifunov <martintrifunov@proton.me>
2024-03-16 15:39:15 +0100

    Add new deal endpoint & update status code/messages

--- server/src/routes/deals.js
+++ server/src/routes/deals.js
@@ -7,9 +7,28 @@ const router = express.Router();
 router.get("/dashboard", async (req, res) => {
   try {
     const response = await DealModel.find({});
-    res.json(response);
+    res.status(200).json(response);
   } catch (error) {
-    res.json(error);
+    res
+      .status(500)
+      .json({ message: "Error getting deals", error: error.message });
+  }
+});
+
+router.get("/dashboard/:id", async (req, res) => {
+  const dealId = req.params.id;
+  try {
+    const getDeal = await DealModel.findById(dealId);
+
+    if (!getDeal) {
+      return res.status(404).json({ message: "Deal not found!" });
+    }
+
+    res.status(200).json(getDeal);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error getting deal", error: error.message });
   }
 });
 
@@ -17,9 +36,11 @@ router.post("/dashboard", async (req, res) => {
   const deal = new DealModel(req.body);
   try {
     const response = await deal.save();
-    res.json(response);
+    res.status(200).json(response);
   } catch (error) {
-    res.json(error);
+    res
+      .status(500)
+      .json({ message: "Error registering deal", error: error.message });
   }
 });
 
