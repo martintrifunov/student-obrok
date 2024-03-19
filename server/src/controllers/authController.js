0b1e3a341267bb6cb340f8765b3bee940174c97d
martintrifunov <martintrifunov@proton.me>
2024-03-19 01:30:52 +0100

    Implement controllers, env variables, refresh tokens

---
+++ server/src/controllers/authController.js
@@ -0,0 +1,55 @@
+import { UserModel } from "../models/Users.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const handleLogin = async (req, res) => {
+  const { user, pwd } = req.body;
+  if (!user || !pwd)
+    return res
+      .status(400)
+      .json({ message: "Username and password are required." });
+
+  const foundUser = await UserModel.findOne({ username: user }).exec();
+  if (!foundUser) return res.sendStatus(401); //Unauthorized
+  // evaluate password
+  const match = await bcrypt.compare(pwd, foundUser.password);
+  if (match) {
+    // create JWTs
+    const accessToken = jwt.sign(
+      {
+        UserInfo: {
+          username: foundUser.username,
+        },
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "10s" }
+    );
+    const refreshToken = jwt.sign(
+      { username: foundUser.username },
+      process.env.REFRESH_TOKEN_SECRET,
+      { expiresIn: "1d" }
+    );
+    // Saving refreshToken with current user
+    foundUser.refreshToken = refreshToken;
+    await foundUser.save();
+
+    // Creates Secure Cookie with refresh token
+    res.cookie("jwt", refreshToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
+      maxAge: 24 * 60 * 60 * 1000,
+    });
+
+    // Send access token to user
+    res.json({ accessToken });
+  } else {
+    res.sendStatus(401);
+  }
+};
+
+const authController = {
+  handleLogin,
+};
+
+export default authController;
---
+++ server/src/controllers/dealsController.js
@@ -0,0 +1,109 @@
+import { DealModel } from "../models/Deals.js";
+
+const getAllDeals = async (req, res) => {
+  const deals = await DealModel.find();
+
+  if (!deals) return res.status(204).json({ message: "No deals found." });
+  
+  res.json(deals);
+};
+
+const createNewDeal = async (req, res) => {
+  if (!req?.body?.locationName) {
+    return res.status(400).json({ message: "Location name is required!" });
+  }
+
+  if (!req?.body?.location) {
+    return res
+      .status(400)
+      .json({ message: "Location coordinates are required!" });
+  }
+
+  if (!req?.body?.description) {
+    return res.status(400).json({ message: "Description is required!" });
+  }
+
+  if (!req?.body?.price) {
+    return res.status(400).json({ message: "Price is required!" });
+  }
+
+  if (!req?.body?.image) {
+    return res.status(400).json({ message: "Cover image is required!" });
+  }
+
+  try {
+    const result = await DealModel.create({
+      locationName: req.body.locationName,
+      location: req.body.location,
+      description: req.body.description,
+      price: req.body.price,
+      image: req.body.image,
+    });
+
+    res.status(201).json(result);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const updateDeal = async (req, res) => {
+  if (!req?.body?.id) {
+    return res.status(400).json({ message: "ID parameter is required." });
+  }
+
+  const deal = await DealModel.findOne({ _id: req.body.id }).exec();
+
+  if (!deal) {
+    return res
+      .status(204)
+      .json({ message: `No deal matches ID ${req.body.id}.` });
+  }
+
+  if (req.body?.locationName) deal.locationName = req.body.locationName;
+  if (req.body?.location) deal.location = req.body.location;
+  if (req.body?.description) deal.description = req.body.description;
+  if (req.body?.price) deal.price = req.body.price;
+  if (req.body?.image) deal.image = req.body.image;
+
+  const result = await deal.save();
+  res.json(result);
+};
+
+const deleteDeal = async (req, res) => {
+  if (!req?.body?.id)
+    return res.status(400).json({ message: "ID parameter is required." });
+
+  const deal = await DealModel.findOne({ _id: req.body.id }).exec();
+
+  if (!deal) {
+    return res
+      .status(204)
+      .json({ message: `No deal matches ID ${req.body.id}.` });
+  }
+  const result = await deal.deleteOne();
+  res.json(result);
+};
+
+const getDeal = async (req, res) => {
+  if (!req?.params?.id)
+    return res.status(400).json({ message: "ID parameter is required." });
+
+  const deal = await DealModel.findOne({ _id: req.params.id }).exec();
+
+  if (!deal) {
+    return res
+      .status(204)
+      .json({ message: `No deal matches ID ${req.params.id}.` });
+  }
+  res.json(deal);
+};
+
+const dealsController = {
+  getAllDeals,
+  createNewDeal,
+  updateDeal,
+  deleteDeal,
+  getDeal,
+};
+
+export default dealsController;
