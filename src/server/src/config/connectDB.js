05a84a74faef94952a1a2fcaf473ae3599f70643
martintrifunov <martintrifunov@proton.me>
2024-04-02 19:15:24 +0200

    Update docker infrastructure, add auth to db

--- src/server/src/config/connectDB.js
+++ src/server/src/config/connectDB.js
@@ -1,8 +1,19 @@
 import mongoose from "mongoose";
+import { UserModel } from "../models/Users.js";
 
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.DATABASE_URI);
+
+    const existingUser = await UserModel.findOne({ username: "uss" }).exec();
+
+    if (!existingUser) {
+      // Create the user if not found
+      UserModel.create({
+        username: process.env.ADMIN_USERNAME,
+        password: process.env.ADMIN_PASSWORD,
+      });
+    }
   } catch (err) {
     console.error(err);
   }
