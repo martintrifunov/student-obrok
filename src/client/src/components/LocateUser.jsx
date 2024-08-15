fcefb87cd1347a3bdfb96d84344dc9d0097a6b5a
martintrifunov <martintrifunov@proton.me>
2024-08-16 00:52:08 +0200

    Add geolocation try count, fail after 5 maximum tries

--- src/client/src/components/LocateUser.jsx
+++ src/client/src/components/LocateUser.jsx
@@ -3,25 +3,43 @@ import { Marker, useMap } from "react-leaflet";
 import L from "leaflet";
 import myLocationMarker from "../assets/icons/my_location_marker.svg";
 
-const LocateUser = ({ onUserLocation, setIsLoading }) => {
+const LocateUser = ({ onUserLocation, setIsLoading, disableRouting }) => {
   const map = useMap();
   const [position, setPosition] = useState(null);
   const firstFlyBy = useRef(true);
+  const retryCount = useRef(0);
+  const maxRetries = 5;
 
   useEffect(() => {
     firstFlyBy.current && setIsLoading(true);
-    const intervalId = setInterval(() => {
-      navigator.geolocation.getCurrentPosition((position) => {
-        const { latitude, longitude } = position.coords;
-        setPosition([latitude, longitude]);
-        onUserLocation([latitude, longitude]);
-        setIsLoading(false);
-        if (firstFlyBy.current) {
-          map.flyTo([latitude, longitude], map.getZoom());
-          firstFlyBy.current = false;
-        }
-      });
-    }, 5000); // update every 5 seconds
+
+    const attemptToGetLocation = () => {
+      if (retryCount.current < maxRetries) {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            const { latitude, longitude } = position.coords;
+            setPosition([latitude, longitude]);
+            onUserLocation([latitude, longitude]);
+            setIsLoading(false);
+            if (firstFlyBy.current) {
+              map.flyTo([latitude, longitude], map.getZoom());
+              firstFlyBy.current = false;
+            }
+          },
+          () => {
+            retryCount.current += 1;
+            if (retryCount.current >= maxRetries) {
+              setIsLoading(false);
+              disableRouting();
+            }
+          }
+        );
+      } else {
+        clearInterval(intervalId);
+      }
+    };
+
+    const intervalId = setInterval(attemptToGetLocation, 5000); // retry every 5 seconds
 
     return () => clearInterval(intervalId); // cleanup on unmount
   }, [map]);
