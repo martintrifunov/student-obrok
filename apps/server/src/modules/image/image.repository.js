ae8fa2c733acc3c395dcc9821ac831ae2fb29e73
martintrifunov <martintrifunov@proton.me>
2026-03-09 20:19:24 +0100

    Implement file service

--- apps/server/src/modules/image/image.repository.js
+++ apps/server/src/modules/image/image.repository.js
@@ -1,15 +1,15 @@
 import { ImageModel } from "../../models/Image.model.js";
 
 export class ImageRepository {
-  async findById(id) {
-    return ImageModel.findById(id).exec();
-  }
-
   async findAll() {
     return ImageModel.find(
       {},
       "title filename url mimeType size createdAt",
     ).exec();
+  }
+
+  async findById(id) {
+    return ImageModel.findById(id).exec();
   }
 
   async create(data) {
