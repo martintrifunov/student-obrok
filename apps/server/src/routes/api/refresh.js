592920a62b570ca73fbb32607ecc3e1ca64c9fac
martintrifunov <martintrifunov@proton.me>
2026-03-09 20:48:08 +0100

    Retire old files after refactor

--- apps/server/src/routes/api/refresh.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { refreshTokenController } from "../../controllers/refreshTokenController.js";
-
-const router = express.Router();
-
-router.get("/refresh", refreshTokenController.handleRefreshToken);
-
-export { router as refreshTokenRouter };
