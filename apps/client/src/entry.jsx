fb2b5e825d58937b53460451f27ac9a514297e1a
martintrifunov <martintrifunov@proton.me>
2026-03-10 04:13:40 +0100

    Wire up react query

--- apps/client/src/entry.jsx
+++ apps/client/src/entry.jsx
@@ -6,17 +6,32 @@ import "@fontsource/roboto/400.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { AuthProvider } from "./context/AuthProvider.jsx";
 import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 if (process.env.NODE_ENV === "production") {
   disableReactDevTools();
 }
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 1,
+      staleTime: 5 * 60 * 1000,
+    },
+  },
+});
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <AuthProvider>
-      <Routes>
-        <Route path="/*" element={<App />}></Route>
-      </Routes>
-    </AuthProvider>
-  </BrowserRouter>
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>,
 );
