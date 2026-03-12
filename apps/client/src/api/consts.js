e0c9e3c18467b6ff0b08cae27d72dff47236c126
martintrifunov <martintrifunov@proton.me>
2026-03-12 04:58:06 +0100

    Remove URI consts from gitignore

---
+++ apps/client/src/api/consts.js
@@ -0,0 +1,5 @@
+export const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+export const BASE_API_URL = `${BASE_URL}/api`;
+export const OSRM_URL =
+  import.meta.env.VITE_OSRM_URL || "http://localhost:5001/route/v1";
+export const RELEASE_VERSION = "Release: β 02.14.2026";
