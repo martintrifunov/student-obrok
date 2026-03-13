7b0c30bf4925ddefa6d28babf76574e7cbe0e2bf
martintrifunov <martintrifunov@proton.me>
2026-03-13 04:16:24 +0100

    Create init ssl automation script

---
+++ init-ssl.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+# init-ssl.sh
+
+domains=(obrok.net)
+rsa_key_size=4096
+data_path="./data/certbot"
+email="your-email@example.com" # REPLACE WITH YOUR EMAIL
+
+if [ -d "$data_path" ]; then
+  read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
+  if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
+    exit
+  fi
+fi
+
+echo "### Downloading recommended TLS parameters ..."
+mkdir -p "$data_path/conf"
+curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
+curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pem"
+
+echo "### Creating dummy certificate for $domains ..."
+path="/etc/letsencrypt/live/$domains"
+mkdir -p "$data_path/conf/live/$domains"
+docker compose -f docker-compose.prod.yml run --rm --entrypoint "\
+  openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
+    -keyout '$path/privkey.pem' \
+    -out '$path/fullchain.pem' \
+    -subj '/CN=localhost'" certbot
+
+echo "### Starting nginx ..."
+docker compose -f docker-compose.prod.yml up --force-recreate -d nginx
+
+echo "### Deleting dummy certificate for $domains ..."
+docker compose -f docker-compose.prod.yml run --rm --entrypoint "\
+  rm -Rf /etc/letsencrypt/live/$domains && \
+  rm -Rf /etc/letsencrypt/archive/$domains && \
+  rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
+
+echo "### Requesting Let's Encrypt certificate for $domains ..."
+domain_args=""
+for domain in "${domains[@]}"; do
+  domain_args="$domain_args -d $domain"
+done
+
+# Request real certs
+docker compose -f docker-compose.prod.yml run --rm --entrypoint "\
+  certbot certonly --webroot -w /var/www/certbot \
+    --email $email \
+    $domain_args \
+    --rsa-key-size $rsa_key_size \
+    --agree-tos \
+    --force-renewal" certbot
+
+echo "### Reloading nginx ..."
+docker compose -f docker-compose.prod.yml exec nginx nginx -s reload
+
+echo "### Booting up the rest of the application..."
+docker compose -f docker-compose.prod.yml up -d
