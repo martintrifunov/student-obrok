cd9e3d23f280563f8e7418fb3d781ece3c910a4b
martintrifunov <martintrifunov@proton.me>
2026-03-13 18:26:59 +0100

    Rework user dot to use a styled box for smoother transitions

--- apps/client/src/features/map/components/UserDot.jsx
+++ apps/client/src/features/map/components/UserDot.jsx
@@ -1,16 +1,16 @@
 import React from "react";
+import { Box, styled } from "@mui/material";
 
-const UserDot = () => (
-  <div
-    style={{
-      width: 20,
-      height: 20,
-      borderRadius: "50%",
-      backgroundColor: "#4285F4",
-      border: "3px solid white",
-      boxShadow: "0 0 10px rgba(0,0,0,0.3)",
-    }}
-  />
-);
+const DotContainer = styled(Box)({
+  width: 20,
+  height: 20,
+  borderRadius: "50%",
+  backgroundColor: "#4285F4",
+  border: "3px solid white",
+  boxShadow: "0 0 10px rgba(0,0,0,0.3)",
+  transition: "transform 1s linear",
+});
+
+const UserDot = () => <DotContainer />;
 
 export default UserDot;
