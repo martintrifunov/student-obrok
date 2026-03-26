56fbf4b735d606411b7d3fbf0350bfced58e56b7
martintrifunov <martintrifunov@proton.me>
2026-03-26 04:36:42 +0100

    fix(scraper): register all scrapers in cron

--- apps/server/src/modules/scraper/scraper.cron.js
+++ apps/server/src/modules/scraper/scraper.cron.js
@@ -1,10 +1,18 @@
 import cron from "node-cron";
 import { VeroScraper } from "./markets/vero.scraper.js";
+import { RamstoreScraper } from "./markets/ramstore.scraper.js";
+import { StokomakScraper } from "./markets/stokomak.scraper.js";
+import { KamScraper } from "./markets/kam.scraper.js";
 
 /**
  * All registered market scrapers.
  */
-const SCRAPERS = [new VeroScraper()];
+const SCRAPERS = [
+  new VeroScraper(),
+  new RamstoreScraper(),
+  new StokomakScraper(),
+  new KamScraper(),
+];
 
 /**
  * Initialises the scraper cron job.
