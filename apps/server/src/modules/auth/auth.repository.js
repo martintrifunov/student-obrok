d96e00c49b3f55f9f93cb6fc41403625588201d2
martintrifunov <martintrifunov@proton.me>
2026-04-18 17:33:37 +0200

    fix(auth): use atomic mongodb operations for refresh token rotation

--- apps/server/src/modules/auth/auth.repository.js
+++ apps/server/src/modules/auth/auth.repository.js
@@ -12,4 +12,42 @@ export class AuthRepository {
   async save(user) {
     return user.save();
   }
+
+  async rotateRefreshToken(userId, oldToken, newToken) {
+    return UserModel.findOneAndUpdate(
+      { _id: userId, refreshToken: oldToken },
+      [
+        {
+          $set: {
+            refreshToken: {
+              $concatArrays: [
+                {
+                  $filter: {
+                    input: "$refreshToken",
+                    cond: { $ne: ["$$this", oldToken] },
+                  },
+                },
+                [newToken],
+              ],
+            },
+          },
+        },
+      ],
+      { returnDocument: "after" },
+    ).exec();
+  }
+
+  async removeRefreshToken(userId, token) {
+    return UserModel.updateOne(
+      { _id: userId },
+      { $pull: { refreshToken: token } },
+    ).exec();
+  }
+
+  async clearRefreshTokens(userId) {
+    return UserModel.updateOne(
+      { _id: userId },
+      { $set: { refreshToken: [] } },
+    ).exec();
+  }
 }
